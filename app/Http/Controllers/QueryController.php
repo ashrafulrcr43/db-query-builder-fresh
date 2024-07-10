c23f6a51e7f88cbb144e0942914901099eec2e05
Devashrafulislam <idb.ashraful@gmail.com>
2024-07-10 12:54:37 +0600

    first commit

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class QueryController extends Controller {
+    function index() {
+        return response('Hello World');
+
+
+    }
+}
